2554896c98b77c9bafa8a4f6775f6c6455fe78ff
Arun_Bikram_Khatri <71219987+ArunBikramkhatri@users.noreply.github.com>
2021-04-14 13:05:53 +0545

    updated

--- housing.py
+++ housing.py
@@ -15,7 +15,10 @@ Created on Mon Apr  5 16:56:47 2021
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
-
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 # Data with salary and ID
 file0 = pd.read_csv("F:/ML Datasets/sample_submission.csv")
@@ -67,27 +70,79 @@ for i in range(len(data.columns)):
 numerical_imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
 categorical_imputer = SimpleImputer(missing_values=np.nan , strategy='most_frequent')
 
+#Replacing nan by most freuent for categorical data
+for i in range(len(categorical_nan.columns)):
+
+    categorical_nan.iloc[:,i] = categorical_imputer.fit_transform(categorical_nan.iloc[:,i].values.reshape(-1,1))
+
+#Replacing nan by mean for numerical data
+for j in range(len(numerical_nan.columns)):
+    numerical_nan.iloc[:,j] = numerical_imputer.fit_transform(numerical_nan.iloc[:,j].values.reshape(-1,1))
 
 
+#concating categprical and numerical data after removing nan values
+full_data = pd.concat([categorical_nan, numerical_nan ],axis=1)
+
+# x = independent variable and y is dependent varaiabl
+x = full_data.iloc[:,:-1]
+y = full_data.iloc[:,-1].values.reshape(-1,1)
+
+#checking if there are values or not
+#a = full_data.isnull().sum()
+
+num_indices = []
+cate_indices = []
 
 
+for i in range(len(x.columns)):
+    if(x.iloc[:,i].dtype == 'O'):
+        cate_indices.append(i)
+    elif(x.iloc[:,i].dtype == "int64" or x.iloc[:,i].dtype == "float64"):
+        num_indices.append(i)
+    
+cate_label_indices = []
+cate_onehot_indices = []
+
+for i in cate_indices:
+    if(len(x.iloc[:,i].value_counts())==2):
+        cate_label_indices.append(i)
+        
+    elif(len(x.iloc[:,i].value_counts()) > 2):
+        cate_onehot_indices.append(i)
+
+label = LabelEncoder()
+for i in cate_label_indices:
+    x.iloc[:,i] = label.fit_transform(x.iloc[:,i])
+    
+#ct = ColumnTransformer(transformers=[('encoder') , OneHotEncoder() , cate_onehot_indices] , remainder='passthrough')
+
+#x = ct.fit_transform(full_data)
+ct = ColumnTransformer([("encoder" , OneHotEncoder() , cate_onehot_indices)] , remainder="passthrough")
+x = ct.fit_transform(x)
+x = x.toarray()
+
+#Feature Scaling
+scaler = StandardScaler()
+x = scaler.fit_transform(x)
+
+#checking score of different models
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
 
 
+lin_val_score = cross_val_score(LinearRegression() , x, y ,cv=10)
+svr_score = cross_val_score(SVR() , x , y, cv = 10)
+
+from sklearn.model_selection import train_test_split
+x_train , x_test , y_train , y_test = train_test_split(x,y ,test_size=0.2 ,random_state=0)
+
+regressor = SVR()
+regressor.fit(x_train , y_train)
+y_pred = regressor.predict(x_test)
 
 
+y_pred = y_pred.reshape(len(y_pred),1)
+y_test = y_test.reshape(len(y_test),1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+concat = np.concatenate((y_pred,y_test),axis=1)
